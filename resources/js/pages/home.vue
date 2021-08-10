4aa77244fe3687e0ef53f49b07398508b0dbe11e
Amzkit <amzkit@gmail.com>
2021-08-10 22:45:56 +0700

    Fix API Authen Error

--- resources/js/pages/home.vue
+++ resources/js/pages/home.vue
@@ -1,24 +1,58 @@
 <template>
     <v-row justify="center">
         <v-col>
-            <v-card>
+            <v-card :loading="loading">
                 <v-card-title>
-                <span class="">Admin Login</span>
+                <span class="">Home Component</span>
                 </v-card-title>
 
                 <v-card-text>
                     I'm a home component from <br>
                     /resources/js/pages/home.vue
+
+                    <v-card v-if="user" class="mt-3">
+                        <v-card-title>Current User</v-card-title>
+                        <v-card-subtitle>Data retrived from api "/api/user"</v-card-subtitle>
+                        <v-card-text>
+                            Name : {{ user.name }} <br>
+                            Email : {{ user.email }} <br>
+                        </v-card-text>
+                    </v-card>
                 </v-card-text>
             </v-card>
         </v-col>
+
+
     </v-row>
 </template>
 
 <script>
-    export default {
-        mounted() {
-            console.log('Component mounted.')
+export default {
+    data: function(){
+        return {
+            loading : true,
+            user: null,
         }
+    },
+    mounted() {
+
+        console.log('Component mounted.')
+    },
+    created(){
+        this.initialize()
+    },
+    methods:{
+        async initialize(){
+            this.loading = true
+            await axios.get('/api/user')
+                .then(response => {
+                    if(response.data.success == true){
+                        console.log(response.data)
+                        this.user = response.data.user
+                    }
+                })
+            this.loading = false
+        },
     }
+}
 </script>
